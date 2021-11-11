dee4091c40320338dcb0d81aa9851f9757baebed
Felipe Pacheco <fpachecomanoel@uol.com.br>
2021-11-11 13:53:37 -0300

    correcao no codigo do grafo

--- source/graph_data.py
+++ source/graph_data.py
@@ -1,9 +1,9 @@
 import csv
 
 def insert_jogador(arquivo,nome,mmr,rank,id,taxa):
-    fieldnames = ['nome', 'mmr', 'rank','id','taxa']
+    fieldnames = ['nome', 'mmr', 'rank','id']
     encontrado=False
-    with open('employee_birthday.txt', mode='r') as csv_file:
+    with open(arquivo, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
         for row in csv_reader:
@@ -15,7 +15,24 @@ def insert_jogador(arquivo,nome,mmr,rank,id,taxa):
         with open(arquivo, mode='w') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerow({'nome': str(nome), 'mmr': str(mmr), 'id': str(id),'rank':str(rank), 'taxa': str(taxa)})
+            writer.writerow({'nome': str(nome), 'mmr': str(mmr), 'id': str(id),'rank':str(rank)})
 
-        
-   
+
+def create_jogado(arquivo,id1,id2):
+    fieldnames = ['jogador 1', 'jogador 2']
+    encontrado=False
+    with open(arquivo, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        linha=0
+        for row in csv_reader:
+            if line_count != 0:
+                if row['jogador1']==id1 and row['jogador2']==id2:
+                    encontrado=True
+                    linha=line_count
+                line_count+=1
+    if not encontrado:
+        with open(arquivo, mode='w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'jogador 1': str(id1), 'jogador 2': str(id2)})
