fd1fda7c141d3ef7262b05632f3fbfa6f51179cf
Felipe Pacheco <fpachecomanoel@uol.com.br>
2021-11-23 12:20:25 -0300

    correção do script de coleta dos herois e adicionado id dos herois

--- src/hero_data.py
+++ src/hero_data.py
@@ -5,7 +5,7 @@ def generate_heroes(arquivo1,arquivo2):
     url='https://raw.githubusercontent.com/odota/dotaconstants/master/build/heroes.json'
     json=requests.get(url)
     json=json.json()
-    fieldnames=['nome','atributo_primario','tipo_ataque']
+    fieldnames=['id','nome','atributo_primario','tipo_ataque']
     fieldnames2=['nome','funcao']
     with open(arquivo1, mode='w') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -13,7 +13,7 @@ def generate_heroes(arquivo1,arquivo2):
         for hero in json:
             print(hero)
             
-            writer.writerow({'nome': json[hero]['localized_name'], 'atributo_primario': json[hero]['primary_attr'],'tipo_ataque':json[hero]['attack_type']})
+            writer.writerow({'id':json[hero]['id'],'nome': json[hero]['localized_name'], 'atributo_primario': json[hero]['primary_attr'],'tipo_ataque':json[hero]['attack_type']})
     with open(arquivo2, mode='w') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames2)
         writer.writeheader()
@@ -23,7 +23,7 @@ def generate_heroes(arquivo1,arquivo2):
 
 def main():
     arq1='hero_data.csv'
-    arq2='.hero_role_data.csv'
+    arq2='hero_role_data.csv'
     generate_heroes(arq1,arq2)
     
 if __name__ == '__main__':
